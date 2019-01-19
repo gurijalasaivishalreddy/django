790f52942df8b4d3c9e39308d26b40ada998482d
vasavi college <info@info.com>
2019-01-19 12:32:46 +0530

    My first or second checknin

---
+++ wordcount/wcount/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import operator
+
+def homepage(request):
+    return render(request,'wcount/home.html')
+def about(request):
+    return render(request,'wcount/about.html')
+def count(request):
+    fulltext=request.GET['text']
+    word_count=len(fulltext.split())
+    word_dict={}
+    for w in fulltext.split():
+        if w in word_dict:
+            word_dict[w]+1
+        else:
+            word_dict[w]=1
+
+    return render(request,'wcount/count.html',{'counttext': fulltext,'word_count':word_count,'word_dict':word_dict})
